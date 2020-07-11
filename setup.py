2c9b47e34a7623c6b39d2d34691d80afd7722de9
Aniket Sharma <aniketsharma552@gmail.com>
2020-07-11 08:37:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='my_tic_tac_toe',
+      version='1.0',
+      description='The Tic Tac Toe python game',
+      long_description='Tic-tac-toe, is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. This is a working Python Package and can be installed either locally or via pip from the pypi repository.',
+      packages=['my_tic_tac_toe'],
+      license='MIT',
+      python_requires='>=3.5',
+      install_requires=[],
+      author='Aniket Sharma',
+      author_email='aniketsharma552@gmail.com',
+      zip_safe=False)
